31638e614395ec2f48638a4a1b5e5f55ae8a6e18
Asmanjas <31342980+Asmanjas@users.noreply.github.com>
2018-10-17 22:50:39 +0530

    base commit.. basic UI ready

--- app/src/main/java/com/a3rdsemproject/asmanjas/smarthome/MainActivity.java
+++ app/src/main/java/com/a3rdsemproject/asmanjas/smarthome/MainActivity.java
@@ -3,18 +3,22 @@ package com.a3rdsemproject.asmanjas.smarthome;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,53 +35,52 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
-    private Toolbar toolbar;
+    private String m_Text = "";
 
-    RecyclerView recyclerView;
-    RecyclerAdapter recyclerAdapter;
-    String s1[];
+private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                home home1 = new home();
+                android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.cons, home1, "home fragment");
+                fragmentTransaction.commit();
+                return true;
+            case R.id.navigation_rooms:
+                Rooms rooms = new Rooms();
+                android.support.v4.app.FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction1.replace(R.id.cons, rooms, "room fragment");
+                fragmentTransaction1.commit();
+                return true;
 
+            case R.id.navigation_automation:
+                Automation automation = new Automation();
+                android.support.v4.app.FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction2.replace(R.id.cons, automation, "automation fragment");
+                fragmentTransaction2.commit();
+                return true;
+        }
+        return false;
+    }
+};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recycler_view_ak);
-        s1 = getResources().getStringArray(R.array.places);
-        recyclerAdapter = new RecyclerAdapter(this,s1);
-
-        recyclerView.setAdapter(recyclerAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        final List<Object> list = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            list.add(new Object());
-        }
-        recyclerAdapter.setItems(list);
-
-
-
-
-
-
-
-
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
+        BottomNavigationView navigationView1 = (BottomNavigationView)findViewById(R.id.bottomNavigationView);
+        navigationView1.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
 
-
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
-        toolbar.setTitleTextColor(Color.WHITE);
-
-
-
-
+//default fragment for home screen
+        home home1 = new home();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.cons, home1, "home fragment");
+        fragmentTransaction.commit();
 
         mDrawerLayout.addDrawerListener(
                 new DrawerLayout.DrawerListener() {
@@ -113,13 +116,22 @@ public class MainActivity extends AppCompatActivity {
 
                 switch (item.getItemId()){
 
-                    case R.id.settings:
-                        Toast.makeText(getApplicationContext(),"u clicked settings",Toast.LENGTH_SHORT).show();
+                    case R.id.ipsetup:
+                        Toast.makeText(getApplicationContext(),"u clicked ipSetup",Toast.LENGTH_SHORT).show();
                         mDrawerLayout.closeDrawers();
                         break;
 
-                    case R.id.checklist:
-                        Toast.makeText(getApplicationContext(),"clicked checkList",Toast.LENGTH_SHORT).show();
+                    case R.id.settings:
+                        Toast.makeText(getApplicationContext(),"clicked settings",Toast.LENGTH_SHORT).show();
+                        mDrawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.setupWizard:
+                        Toast.makeText(getApplicationContext(),"clicked setup wizard",Toast.LENGTH_SHORT).show();
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.credits:
+                        mDrawerLayout.closeDrawers();
                         break;
 
                 }
@@ -131,11 +143,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.simple_menu,menu);
-        return true;
-    }
 
 
     @Override
@@ -150,9 +157,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void onClickInfo(MenuItem item){
-        //Toast.makeText(this,"you clicked info button",Toast.LENGTH_SHORT).show();
 
+
+
+
+    public void onClickInfo(MenuItem item){//to show credits
         AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
         builder1.setMessage("Asmanjas, Suman, Shweta\n3rd Semester, MCA\nNITK, Surathkal");
         builder1.setTitle("Developed by");
@@ -168,6 +177,7 @@ public class MainActivity extends AppCompatActivity {
 
         AlertDialog alert11 = builder1.create();
         alert11.show();
+        mDrawerLayout.closeDrawers();
 
     }
 
@@ -176,4 +186,60 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+    public void ipSetup(MenuItem item){//get the ip setup dialog on screen
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //builder.
+        builder.setTitle("enter IP address of MCU device");
+
+// Set up the input
+        final EditText input = new EditText(this);
+        input.setHint(R.string.ipHint);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        //input.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        builder.setView(input);
+        input.setPadding(20,20,20,20);
+
+// Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+
+    //master bedroom click
+    public void onClickMasterBedroom(View view){
+        Master_Bedroom ms = new Master_Bedroom();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragcons,ms, "master bedroom fragment");
+        fragmentTransaction.commit();
+    }
+
+    //home screen good night function
+    public void onClickGoodNightAll(View view) {
+    }
+
+
+
+    //home Screen good morning function
+    public void onClickGoodMorningAll(View view) {
+
+    }
+
+    //holiday button
+    //on click holiday,turn all lights and fans off and turn on security system i.e. motion sensor
+    public void onClickHoliday(View view) {
+    }
 }
---
+++ app/src/main/java/com/a3rdsemproject/asmanjas/smarthome/Master_Bedroom.java
@@ -0,0 +1,29 @@
+package com.a3rdsemproject.asmanjas.smarthome;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Master_Bedroom extends Fragment {
+
+
+    public Master_Bedroom() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_master__bedroom, container, false);
+    }
+
+}
